5561b5bb2eb63540da9caa41b93670e7482b85d6
sadam <sadamccr@gmail.com>
2022-12-14 02:07:36 +0500

    list/arrays done

--- main.py
+++ main.py
@@ -22,4 +22,50 @@
 #
 # print(search_value(2000))
 #
+#
+# expenses = [2200, 2350, 2600, 2130, 2190]
+# june_expense = 1980
+# expenses.append(june_expense)
+#
+# print(expenses)
+#
+# refund = 200
+# expenses[3] = expenses[3] - refund
+# print(expenses)
+
+# #-------------------------------
+#
+# heroes = ['spider man', 'thor', 'hulk', 'iron man', 'captain america']
+#
+# print(len(heroes))
+#
+# heroes.append('black panther')
+#
+# print(heroes)
+#
+# heroes.pop(-1)
+# print(heroes)
+# heroes.insert(3, 'black panther')
+# print(heroes)
+# heroes[1:3] = ['doctor cool']
+# print(heroes)
+#
+# # print(dir(heroes))
+#
+# heroes.sort()
+# print(heroes)
+#
+# heroz = sorted(heroes)
+# print(heroz)
+
+#
+# max = int(input("Enter max number: "))
+#
+# odd_numbers = []
+#
+# for i in range(1, max):
+#     if i % 2 == 1:
+#         odd_numbers.append(i)
+#
+# print("Odd numbers: ", odd_numbers)
 
