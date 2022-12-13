52eaf417c1867c609b6b6ae9a3af11b59ab6877e
sadam <sadamccr@gmail.com>
2022-12-14 01:35:46 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# expenses = [2200, 2350, 2600, 2130, 2190]
+#
+# extra_expenses = expenses[1] - expenses[0]
+# print(f'Extra expense = {extra_expenses}')
+#
+# # #--------------------------------
+# first_quarter_expenses = 0
+# for i, v in enumerate(expenses):
+#     if i < 3:
+#         first_quarter_expenses += v
+#     else:
+#         break
+# print(f'First Quarter Expenses = {first_quarter_expenses}')
+#
+#
+# def search_value(num):
+#     if num in expenses:
+#         return True
+#     else:
+#         return False
+#
+#
+# print(search_value(2000))
+#
+
