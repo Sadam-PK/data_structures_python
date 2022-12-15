760bd30932304eecf94f95e5ae79c8670fde2a12
sadam <sadamccr@gmail.com>
2022-12-15 12:49:10 +0500

    node inserted at head of linkedlist done

--- main.py
+++ main.py
@@ -1,71 +1,32 @@
-# expenses = [2200, 2350, 2600, 2130, 2190]
-#
-# extra_expenses = expenses[1] - expenses[0]
-# print(f'Extra expense = {extra_expenses}')
-#
-# # #--------------------------------
-# first_quarter_expenses = 0
-# for i, v in enumerate(expenses):
-#     if i < 3:
-#         first_quarter_expenses += v
-#     else:
-#         break
-# print(f'First Quarter Expenses = {first_quarter_expenses}')
-#
-#
-# def search_value(num):
-#     if num in expenses:
-#         return True
-#     else:
-#         return False
-#
-#
-# print(search_value(2000))
-#
-#
-# expenses = [2200, 2350, 2600, 2130, 2190]
-# june_expense = 1980
-# expenses.append(june_expense)
-#
-# print(expenses)
-#
-# refund = 200
-# expenses[3] = expenses[3] - refund
-# print(expenses)
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
 
-# #-------------------------------
-#
-# heroes = ['spider man', 'thor', 'hulk', 'iron man', 'captain america']
-#
-# print(len(heroes))
-#
-# heroes.append('black panther')
-#
-# print(heroes)
-#
-# heroes.pop(-1)
-# print(heroes)
-# heroes.insert(3, 'black panther')
-# print(heroes)
-# heroes[1:3] = ['doctor cool']
-# print(heroes)
-#
-# # print(dir(heroes))
-#
-# heroes.sort()
-# print(heroes)
-#
-# heroz = sorted(heroes)
-# print(heroz)
 
-#
-# max = int(input("Enter max number: "))
-#
-# odd_numbers = []
-#
-# for i in range(1, max):
-#     if i % 2 == 1:
-#         odd_numbers.append(i)
-#
-# print("Odd numbers: ", odd_numbers)
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
+    def insert_begining(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print('Linkedlist is empty')
+            return
+        else:
+            itr = self.head
+            llstr = ''
+            while itr:
+                llstr += str(itr.data) + ' --> '
+                itr = itr.next
+            print(llstr)
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert_begining(5)
+    ll.insert_begining(89)
+    ll.print()
